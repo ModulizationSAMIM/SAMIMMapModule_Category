ca25d119113112b8874d89c0a9cf62728038fd97
Youhui_iOS <guhuangwudi@gmail.com>
2017-09-28 11:57:03 +0800

    Initial Commit

---
+++ SAMIMMapModule_Category/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SAMIMMapModule_Category
+//
+//  Created by ZIKong on 2017/9/28.
+//  Copyright © 2017年 youhuikeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
